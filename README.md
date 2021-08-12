558796c6abe3f5a9470559651f07a5a9d30dee5b
2mill <yannick.dorn@gmail.com>
2021-08-12 16:57:00 -0400

    working on printing the list, trying to figure out why comments are not being read...

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Python Thing
 A quick script for me to organize port information data.
+
+
+## CSV information
+CSV files should be split as such
+`Room Number,Box Label,Port Count,Used,Patch Panel,Patch Port,Parent Room,Comments`.
+The program will fail, or crash if it is not met. More information to be added later.
--- terminal_reader.py
+++ terminal_reader.py
@@ -1,21 +1,24 @@
 # Meant to process the CSV file information.
-import sys, argparse
+import sys, argparse, csv
+from typing import Union
 
 parser = argparse.ArgumentParser(description="A data processor for a Terminal list")
 
-csv_file = 
+# csv_file = 
 
 
-def XandSlashEnum(symbol: str) -> int:
-	return 1 if symbol is 'X' else 0
+def translate_used_symbols(symbol: str) -> int:
+	return 1 if symbol == 'X' else 0
 
 # function for converting from my markings into a designation
-def translate(port_usage: str) -> int:
+def translate_symbols_to_bin(port_usage: str) -> Union[int, bool]:
+	if port_usage == "undefined": return False
 	temp: list[str] = port_usage.split(' ')
-	sum: int = 0;
+	temp.reverse()
+	total: int = 0;
 	for x in range(len(temp)):
-		sum = sum + (2**x)*(XandSlashEnum(temp[x]))
-	return sum
+		total = total + (2**x)*(translate_used_symbols(temp[x]))
+	return total
 
 	##This should return the sequence
 
@@ -35,10 +38,35 @@ class Terminal:
 		self.room = room
 		self.box_label = box_label
 		self.port_count = port_count
-		self.used = translate(used)
+		self.used = translate_symbols_to_bin(used)
 		self.patch_port = patch_port
 		self.parent_room = parent_room
 		self.comment = comment
-def create_list(filename:str) -> list[Terminal]:
+
+	def __str__(self) -> str:
+		return f"\t\n{self.room}\t\n{self.box_label}\t\n{self.port_count}\t\n{self.used}\t\n{self.patch_port}\t\n{self.comment}"
+	def get_parent_room(self) -> str:
+		return self.parent_room
+def create_list(filename:str) -> list:
 	file = open(filename, "r")
+	# Remove header information
+	file.readline()
 	file = file.readlines()
+	terminal_list: list = []
+	for line in file:
+		info: list = line.split(',')
+		#Remove trailing whitespace
+		info.pop()
+		print(info)
+		terminal_list.append(Terminal(*info))
+	return terminal_list
+
+def string_terminal_list(terminal_list: list) -> str:
+	builder: str = ""
+	for terminal in terminal_list:
+		parent_room: str = terminal.parent_room 
+		print(f"{parent_room}, {terminal.comment}")
+
+
+temp: list = create_list(sys.argv[1])
+# string_terminal_list(temp)
