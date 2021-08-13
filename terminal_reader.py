844ea36f7c1bd5e863bbc8c76e476f77c4d93e93
Yannick <yannick.dorn@gmail.com>
2021-08-13 13:45:20 -0400

    got CSV working

--- terminal_reader.py
+++ terminal_reader.py
@@ -4,8 +4,11 @@ from typing import Union
 
 parser = argparse.ArgumentParser(description="A data processor for a Terminal list")
 
-# csv_file = 
 
+def is_header(csv_line: list) -> bool:
+	for index in csv_line:
+		if index == "Room Number": return True
+		else: return False
 
 def translate_used_symbols(symbol: str) -> int:
 	return 1 if symbol == 'X' else 0
@@ -25,48 +28,34 @@ def translate_symbols_to_bin(port_usage: str) -> Union[int, bool]:
 
 
 class Terminal:
-	def __init__(
-		self,
-		room: str, 
-		box_label: str,
-		port_count: int,
-		used:str,
-		patch_port: str,
-		parent_room: str,
-		comment: str,
-		) -> None:
-		self.room = room
-		self.box_label = box_label
-		self.port_count = port_count
-		self.used = translate_symbols_to_bin(used)
-		self.patch_port = patch_port
-		self.parent_room = parent_room
-		self.comment = comment
+	def __init__(self, info) -> None:
+		self.room, self.box_label, self.port_count, self.port_count, self.used, self.patch_port, self.parent_room, self.comment = info
+		self.used = translate_symbols_to_bin(self.used)
 
 	def __str__(self) -> str:
 		return f"\t\n{self.room}\t\n{self.box_label}\t\n{self.port_count}\t\n{self.used}\t\n{self.patch_port}\t\n{self.comment}"
 	def get_parent_room(self) -> str:
 		return self.parent_room
 def create_list(filename:str) -> list:
-	file = open(filename, "r")
-	# Remove header information
-	file.readline()
-	file = file.readlines()
-	terminal_list: list = []
-	for line in file:
-		info: list = line.split(',')
-		#Remove trailing whitespace
-		info.pop()
-		print(info)
-		terminal_list.append(Terminal(*info))
-	return terminal_list
+	# DO NOT REMOVE THIS UTF ENCODING
+	with open(f"./{filename}", encoding="utf-8-sig") as f:
+		reader = csv.reader(f)
+		terminal_list: list = []
+		for row in reader:
+			if is_header(row): continue
+			terminal_list.append(Terminal(row))
+		return terminal_list
+
 
 def string_terminal_list(terminal_list: list) -> str:
 	builder: str = ""
 	for terminal in terminal_list:
 		parent_room: str = terminal.parent_room 
-		print(f"{parent_room}, {terminal.comment}")
+		print(f"{parent_room},{terminal.comment}")
+
 
 
 temp: list = create_list(sys.argv[1])
+
+string_terminal_list(temp)
 # string_terminal_list(temp)
