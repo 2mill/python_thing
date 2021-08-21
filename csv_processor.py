2b993b1694b634d8733a48b2dcc1154da3d3e038
2mill <yannick.dorn@gmail.com>
2021-08-21 18:33:13 -0400

    Working on making automated tests for the csv processor. Currently working on port information. Probably putting too much work into this part

--- csv_processor.py
+++ csv_processor.py
@@ -39,3 +39,15 @@ def translate_symbols_to_bin(port_usage) -> Union[int, bool]:
 	for x in range(len(temp)):
 		total = total + (2**x)*(translate_used_symbols(temp[x]))
 	return total
+
+def translate_patch_ports(panel_info: str)-> list:
+	panel_info.replace(" ", "")
+	first: list = panel_info.split("-")
+	output: list = []
+	for item in first:
+		output.append(item.split("."))
+
+	# If a split occurs because of a -, then there are more than one
+	# however, if there is no - in the split, then we just need to pop
+	# the first value.
+	return output if len(output) > 1 else output.pop()
--- tests/csv_process_tests.py
+++ tests/csv_process_tests.py
@@ -1,4 +1,8 @@
 import csv_processor
+import random
+
+
+passed = lambda x: "FAIL" if x else "PASS"
 
 def test_conversions() -> bool :
 	result_bool = False
@@ -8,3 +12,34 @@ def test_conversions() -> bool :
 	test_csv_file: list = [['169G', '169G-1', '2', '/ /', '12', '8.9 - 17.18', '148', '']]
 	result_bool = csv_processor.process(test_csv_file) == [['169G', '169G-1', '2', 0, '12', [['8', '9'], ['17', '18']], '148', '']]
 	return result_bool
+
+
+def get_random_set() -> str:
+	return f"{random.randrange(1,48)}.{random.randrange(1,48)}"
+
+# Returns the string attempt
+def random_list_and_formatted(size: int) -> str:
+	other_temp: str = ""
+	for i in range(size):
+		number = random.randrange(1,49)
+		# BAD CODE GOES BRRT
+		other_temp = f"{number}" if len(other_temp) == 0 else f"{other_temp}.{number}"
+
+	return other_temp
+	
+
+def test_patchinfo_translation_single(size) -> None:
+	tester: str = random_list_and_formatted(4) 
+
+	# TODO: REBUILD THIS PART TO TAKE THE OUTPUT
+	# FROM THE PREVIOUS FUNCTION AND FORMAT IT SO THAT TRIES TO MATCH	
+	pass
+
+
+	# Model
+	# "12.13" -> ["12", "13"]
+	# "12.13 - 12.13" -> [["12", "13"], ["12, 13"]]
+
+
+def test_patchinfo_translation_double() -> bool:
+	pass
