414b8ab1663cc1f72075bb4dad8f6a34ef6ef9a2
Yannick Dorn <yannick.dorn@gmail.com>
2021-08-29 02:54:53 -0400

    Added some notes into my code cuz it's late, and not in the mood to add it to an extra change log

--- structs/terminal.py
+++ structs/terminal.py
@@ -14,6 +14,20 @@ class Port:
 
 def get_header_info() -> list:
 	return ["room", "box_label", "port_count", "used", "patch_panel", "patch_port", "parent_room", "comment"]
+# This just needs to generate links for the Ports and it's Parent Room.
+# The Parent Room therefore should have links to its terminal
+# This is somewhat starting to look like trees lol.
+# CREATOR LOGS AND THOUGHTS
+# My whole data structure is starting to look like a GENERAL TREE with back and forth links
+# for each child. This means that the general structure would look like this.
+# Parent Room Node -> Room Nodes ? idk I might need to code this, props not, (next line cont...)
+# cont... -> Terminal -> Port -> Patch Panel Node -> Patch Panel
+# HOWEVER. The Patch Panel would be it's own node to links for Patch Panel Nodes.
+# This means that it's more of a graph than anything.
+# And data can be reached from every part which does not make a lot of sense.
+# I think I need to draw out the structure and see where I can make changes.
+# Also making sense of how this would reflect in the user interface will help a lot.
+# More notes on this later....
 class Terminal:
 	def __init__(self, info) -> None:
 
